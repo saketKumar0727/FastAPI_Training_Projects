8498bbededd2dab626f8f7445d61fc4350e0b1ba
git_practice_sk <saketkr0727@gmail.com>
2026-04-03 12:18:19 +0530

    Updated the database to sqlite3 and added phone_number feild in Users table and one put api for phone number change

--- TodoApp/database.py
+++ TodoApp/database.py
@@ -2,8 +2,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:test1234@127.0.0.1:3306/TodoApplicationDatabase'
-# SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'   for sqlite database
+# SQLALCHEMY_DATABASE_URL = 'mysql+pymysql://root:test1234@127.0.0.1:3306/TodoApplicationDatabase'
+SQLALCHEMY_DATABASE_URL = 'sqlite:///./todosapp.db'  # for sqlite database
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
