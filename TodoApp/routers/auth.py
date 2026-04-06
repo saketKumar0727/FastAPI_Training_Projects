38991dbd44aa839225344079a1f2f369681bbe52
git_practice_sk <saketkr0727@gmail.com>
2026-04-06 12:42:29 +0530

    Root directory change

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -5,8 +5,8 @@ from pydantic import BaseModel
 from typing import Annotated
 from sqlalchemy.orm import Session
 from starlette import status
-from database import SessionLocal
-from models import Users
+from ..database import SessionLocal
+from ..models import Users
 from passlib.context import CryptContext
 from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer
 from jose import jwt, JWTError
--- TodoApp/routers/users.py
+++ TodoApp/routers/users.py
@@ -3,8 +3,8 @@ from fastapi import  Depends, HTTPException, Path, APIRouter
 from pydantic import BaseModel, Field
 from sqlalchemy.orm import Session
 from starlette import status
-from models import Todos, Users
-from database import SessionLocal
+from ..models import Todos, Users
+from ..database import SessionLocal
 from .auth import get_current_user
 from passlib.context import CryptContext
 
